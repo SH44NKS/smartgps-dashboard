2ffa778ff693eadf823422ebc936ef37a483b51c
SH44NKS <alissons.silva25@gmail.com>
2026-04-24 10:40:00 -0300

    Update smartgps.js

--- api/smartgps.js
+++ api/smartgps.js
@@ -1,6 +1,6 @@
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   return res.status(200).json({
     status: 1,
     message: "Backend SmartGPS funcionando na Vercel"
   });
-};
+}
